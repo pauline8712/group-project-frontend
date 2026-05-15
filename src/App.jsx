2e576b332db55d6cf3e56241dff4ec1422754a54
Pauline <plinekaeorg@gmail.com>
2026-05-15 17:21:27 +0200

    feat: add TransactionForm route to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Categories from './pages/Categories';
+import Dashboard from './pages/Dashboard';
+import TransactionForm from './pages/TransactionForm';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Categories />} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/add-transaction" element={<TransactionForm />} />
       </Routes>
     </BrowserRouter>
   );
